b29cbfdc9ae8f91ed804880e04bff230ac5440b8
David <david@spektrakel.de>
2017-06-23 20:51:36 +0200

    fix: bound properties must be public
    Property '{name-of-property}' is private and only accessible within class '{component-class}'

--- src/app/bttf/bttf.component.ts
+++ src/app/bttf/bttf.component.ts
@@ -9,7 +9,7 @@ import { BttfService } from './bttf.service';
 })
 export class BttfComponent implements OnInit {
 
-  private message: string = `Back to the future, again!`;
+  message: string = `Back to the future, again!`;
 
   count: number = 123;
   items: string = `chickens`;
@@ -27,7 +27,7 @@ export class BttfComponent implements OnInit {
     this.count += 1;
   }
 
-  private onSubmit(value: string) {
+  onSubmit(value: string) {
     this.answer = this.service.ask(value);
   }
 
